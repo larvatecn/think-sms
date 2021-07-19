7baee1e24ea19bcb35d6d3484d21cce18ad9e33d
XuTongle <xutongle@msn.com>
2021-07-19 15:02:18 +0800

    update function

--- src/helpers.php
+++ src/helpers.php
@@ -9,12 +9,15 @@ use Overtrue\EasySms\EasySms;
 use Overtrue\EasySms\Exceptions\NoGatewayAvailableException;
 use Overtrue\EasySms\Exceptions\InvalidArgumentException;
 
-if (!function_exists('sms_send')) {
+if (!function_exists('sms')) {
     /**
+     * 发送短信
+     * @param \Overtrue\EasySms\PhoneNumber|string $mobile
+     * @param string|\Overtrue\EasySms\Message $message
      * @return array|EasySms
      * @throws NoGatewayAvailableException|InvalidArgumentException
      */
-    function sms_send()
+    function sms()
     {
         $arguments = func_get_args();
         /** @var EasySms $sms */
